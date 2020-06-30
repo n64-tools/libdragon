c56c83f8d6378d828eb4e9c713d0567b0c8ba9f7
Robin Jones <networkfusion@users.noreply.github.com>
2020-06-30 01:21:01 +0100

    try different offset for high res...

--- src/display.c
+++ src/display.c
@@ -298,7 +298,7 @@ void display_init_ex( tvtype_t tv, resolution_t res, bitdepth_t bit, uint32_t nu
             __width = 512;
             __height = 240;
             __registers[1] = 512; /* base offset for field 1 */
-            __registers[1 + REGISTER_COUNT] = 1024; /* base offset for field 2 */ //TODO: probably wrong
+            __registers[1 + REGISTER_COUNT] = 240; /* base offset for field 2 */ //TODO: probably wrong
             __registers[2] = 512; /* width */
             __registers[12] = 0x00000333; /* x-scale */
             break;
@@ -306,7 +306,7 @@ void display_init_ex( tvtype_t tv, resolution_t res, bitdepth_t bit, uint32_t nu
             __width = 640;
             __height = 240;
             __registers[1] = 640; /* base offset for field 1 */
-            __registers[1 + REGISTER_COUNT] = 1280; /* base offset for field 2 */ //TODO: probably wrong
+            __registers[1 + REGISTER_COUNT] = 640; /* base offset for field 2 */ //TODO: probably wrong
             __registers[2] = 640; /* width */
             __registers[12] = 0x00000400; /* x-scale */
             break;
